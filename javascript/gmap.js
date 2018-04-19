9948ba29320e9df4d1e2a119db17efe74e58eb5f
SaverLINK48 <jgarci52@hawk.iit.edu>
2018-04-18 23:46:38 -0500

    Added function to quick place to multiple locations.

--- javascript/gmap.js
+++ javascript/gmap.js
@@ -2,14 +2,37 @@ var map;
 var pin;
 var locationm;
 var mexicocity={lat: 19.3910038, lng: -99.1536997}
+var kyotojapan={lat: 35.0061653, lng: 135.7259305};
+var chicagous={lat: 41.8339042, lng: -88.012152};
+var romeitaly={lat: 41.9674596, lng: 12.3930454};
+var locations=["mexicocity","kyotojapan","chicagous","romeitaly"];
 function myMap() {
-	locationm={lat: 19.3910038, lng: -99.1536997};
-	map = new google.maps.Map(document.getElementById('map'), {
-		zoom:10,
-	center: mexicocity});
+  locationm={lat: 19.3910038, lng: -99.1536997};
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom:10,
+  center: mexicocity});
+  pin = new google.maps.pin({
+    position:mexicocity,
+    animation:google.maps.Animation.DROP,
+  map: map });
+}
+function travel(location, myplaces) {
+	map.panTo(location);
+	map.setZoom(10);
 	pin = new google.maps.pin({
-		position:mexicocity,
-		animation:google.maps.Animation.DROP,
-	map: map });
+		position: location,
+			animation:google.maps.Animation.DROP,
+		map: map
+	});
+	if(myplaces==locations[0]){
+	}
+	else if(myplaces==locations[1]){
+	}
+	else if(myplaces==locations[2]){
+	}
+	else if(myplaces==locations[3]) {
+	}
+	else {
+	}
 }
 
