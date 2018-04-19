f9c1bf3d412fb07e7b900d7d3d9146053c43bd42
SaverLINK48 <jgarci52@hawk.iit.edu>
2018-04-18 23:24:34 -0500

    Redid the .js file and established variables with a starter location for the map.

--- javascript/gmap.js
+++ javascript/gmap.js
@@ -1,23 +1,15 @@
-<!DOCTYPE html>
-<html>
-  <body>
-    <h1>My First Google Map</h1>
-    <div id="map" style="width:400px;height:400px;background:yellow"></div>
-    <script>
-      function myMap() {
-      var mapOptions = {
-        center: new google.maps.LatLng(19.3910038, -99.1536997),
-        zoom: 10,
-        mapTypeId: google.maps.MapTypeId.HYBRID
-                       }
-        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-                       }
-        var pin = new google.maps.Marker({
-        position:chicago_il,
-        animation:google.maps.Animation.BOUNCE,
-        map: map });
-    </script>
-    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCMn9W7-6KgR3FW0gfZLPFR96jcvaKJkrQ&callback=myMap">
-    </script>
-</body>
-</html>
+var map;
+var pin;
+var locationm;
+var mexicocity={lat: 19.3910038, lng: -99.1536997}
+function myMap() {
+	locationm={lat: 19.3910038, lng: -99.1536997};
+	map = new google.maps.Map(document.getElementById('map'), {
+		zoom:10,
+	center: mexicocity});
+	pin = new google.maps.pin({
+		position:mexicocity,
+		animation:google.maps.Animation.DROP,
+	map: map });
+}
+
